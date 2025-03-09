e36e75294fab90dbda9dbc0fb871e574027297b2
tltrus <77125487+tltrus@users.noreply.github.com>
2025-03-09 12:24:21 +0400

    up

---
+++ Configuration-model/Graph/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace MathGraph
+{
+    
+    public partial class MainWindow : Window
+    {
+
+        Random rnd = new Random();
+        DispatcherTimer timer = new DispatcherTimer();
+        int width, height;
+        public static DrawingVisual visual;
+        DrawingContext dc;
+
+        List<int> schema = new List<int>{ 3, 3, 3, 2, 4, 4, 5, 3, 4 };
+        List<Node> nodes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            visual = new DrawingVisual();
+
+            width = (int)g.Width;
+            height = (int)g.Height;
+
+            Init();
+
+            timer.Tick += Tick;
+            timer.Interval = new TimeSpan(0,0,0,0,500);
+        }
+
+        private void Init()
+        {
+            schema = GetRandomSchema();
+            nodes = new List<Node>();
+
+            rtbConsole.Document.Blocks.Clear();
+            rtbConsole.AppendText("Schema is [" + String.Join(",", schema) + "]\n");
+
+            for (int i = 0; i < schema.Count; ++i)
+            {
+                double x = rnd.Next(20, width - 20);
+                double y = rnd.Next(20, height - 20);
+
+                Node node = new Node(schema[i]);
+                node.pos = new Point(x, y);
+                node.id = i;
+                node.ConnectedNotify += (str) =>
+                {
+                    rtbConsole.AppendText(str);
+                };
+
+                nodes.Add(node);
+            }
+
+            foreach(var n in nodes)
+            {
+                n.SetConnections(nodes);
+            }
+
+            Drawing();
+        }
+
+        private List<int> GetRandomSchema()
+        {
+            List<int> result = new List<int>();
+
+            int rnd_count = rnd.Next(5, 15);
+            for (int i = 0; i < rnd_count; ++i)
+            {
+                int rnd_value = rnd.Next(1, 10);
+                result.Add(rnd_value);
+            }
+
+            return result;
+        }
+
+        private void Tick(object sender , EventArgs e) => Drawing();
+
+        private void Drawing()
+        {
+            g.RemoveVisual(visual);
+            using (dc = visual.RenderOpen())
+            {
+                foreach (Node node in nodes)
+                {
+                    node.DrawingEdges(dc, nodes);
+                }
+
+                foreach (Node node in nodes)
+                {
+                    node.DrawingNodes(dc);
+                }
+
+                dc.Close();
+                g.AddVisual(visual);
+            }
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            Init();
+            Drawing();
+        }
+    }
+}
---
+++ Configuration-model/Graph/Node.cs
@@ -0,0 +1,93 @@
+﻿using System.Windows.Media;
+using System.Windows;
+using System.Collections.Generic;
+using System.Globalization;
+using System;
+using System.Linq;
+using System.CodeDom;
+using System.Windows.Documents;
+
+namespace MathGraph
+{
+    class Node
+    {
+        public delegate void ConnectedHandle(string str);
+        public event ConnectedHandle ConnectedNotify;
+
+        public List<int> connections; // indexes of Graph nodes list
+        public Point pos;
+        int N; // num of connections
+        public int id;
+
+        public bool IsEmptyForConnection
+        {
+            get
+            {
+                return connections.Count < N;
+            }
+        }
+
+        public Node(int n)
+        {
+            N = n;
+            connections = new List<int>();
+        }
+
+        public void SetConnections(List<Node> nodes)
+        {
+            for (int j = 0; j < nodes.Count; ++j)
+            {
+                if (j == id) continue; // skip if current node is in nodes list
+
+                Node selected_node = nodes[j];
+
+                bool isEmpty = selected_node.IsEmptyForConnection;
+                bool isNotContain = !connections.Contains(selected_node.id);
+
+                if (isEmpty && isNotContain && connections.Count < N)
+                {
+                    connections.Add(selected_node.id);              // save index of node from nodes list
+                    nodes[selected_node.id].connections.Add(id);    // save index of current node
+                }
+            }
+
+            if (connections.Count <= 0) return;
+
+            string str = String.Join(",", connections.Select(i => i.ToString()).ToArray());
+            ConnectedNotify?.Invoke("\nNode " + id + " is connected to nodes [" + str + "]");
+        }
+
+        private double GetDist(Point p)
+        {
+            return Math.Sqrt(Math.Pow(pos.X - p.X, 2) + Math.Pow(pos.Y - p.Y, 2));
+        }
+
+        public void DrawingEdges(DrawingContext dc, List<Node> nodes)
+        {
+            // Draw edges
+            for (int i = 0; i < connections.Count; ++i)
+            {
+                Pen pen = new Pen(Brushes.LightGray, 1);
+
+                int index = connections[i]; // get index of node inside connections array
+                dc.DrawLine(pen, pos, nodes[index].pos);
+
+            }
+        }
+
+        public void DrawingNodes(DrawingContext dc)
+        {
+            // Draw point
+            double size = N + 2;
+            Brush brush = new SolidColorBrush(Color.FromArgb(255, 1, 64, 225));
+            dc.DrawEllipse(Brushes.Black, new Pen(Brushes.LightGray, 3), pos, size, size);
+
+            // Draw labeling
+            FormattedText formattedText = new FormattedText(id.ToString(), CultureInfo.GetCultureInfo("en-us"),
+                                                            FlowDirection.LeftToRight, new Typeface("Verdana"), 11, Brushes.Black,
+                                                            VisualTreeHelper.GetDpi(MainWindow.visual).PixelsPerDip);
+            dc.DrawText(formattedText, new Point(pos.X, pos.Y - 20));
+        }
+    }
+
+}
